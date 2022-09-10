51ec4f58b973dfbcebcf122b602f290330f1ea15
houssam chakir <thisischchannel@gmail.com>
2022-09-10 17:48:01 +0100

    list creating done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
   validates :comment, length: { minimum: 5 }
-  validates :movie_id, uniqueness: { scope: :list_id }, presence: true
+  validates :list_id, uniqueness: { scope: :movie_id }, presence: true
 end

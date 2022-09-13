cf75ccd982cbc0df51a1de985212a59a285e4565
houssam chakir <thisischchannel@gmail.com>
2022-09-13 20:40:04 +0100

    added index to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :lists, except: [:edit, :update] do
+  root to: 'lists#index'
+  resources :lists, except: [:index, :edit, :update] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]

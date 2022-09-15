ebe3fbcbe25272d9f5e9915dfb0b2ada7162c6b4
houssam chakir <thisischchannel@gmail.com>
2022-09-15 19:26:30 +0100

    fixed show issue"

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,35 @@
 class ListsController < ApplicationController
+before_action :select_list, only: %i[show destroy]
+
   def index
     @lists = List.all
   end
+
   def new
     @list = List.new
   end
+
   def create
     @list = List.new(list_params)
     @list.save!
     redirect_to list_path(@list.id)
   end
+
   def show
-    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to root_path, status: :see_other
   end
 
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def select_list
+    @list = List.find(params[:id])
   end
 end
